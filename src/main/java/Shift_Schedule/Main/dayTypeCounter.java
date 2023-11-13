7d542b92b2091f44a63d5c81374ced07e1b83f9f
AuburnWu <hunterwu0530@yahoo.com>
2023-11-13 16:32:34 +0800

    Merge branch 'main' of https://github.com/AuburnWu/shift_Schedule.git

--- src/main/java/Shift_Schedule/Main/dayTypeCounter.java
+++ src/main/java/Shift_Schedule/Main/dayTypeCounter.java
@@ -1,24 +1,38 @@
 package Shift_Schedule.Main;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class dayTypeCounter {
 
-	public static void main(String[] arg) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("E yyyy/MM/dd");
-		
-		DateFormat df = DateFormat.getDateInstance();
-		         Date date = df.parse("2009/1/1");
-		         Calendar calendar = Calendar.getInstance();
-		         calendar.setTime(date);
-		         System.out.println(sdf.format(calendar.getTime()));
-		 String tm = System.currentTimeMillis().toString();
-		
-		System.out.println(sdf);
-	}
+    public int[] dayTypeCount(int year ,int month) {
+    	
+//        int year = 2023;
+//        int month = 12; 
 
+        int daysInMonth = LocalDate.of(year, month, 1).lengthOfMonth();
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+
+        int workdays = 0;
+        int holidays = 0;
+
+        for (int i = 0; i < daysInMonth; i++) {
+            LocalDate currentDate = firstDayOfMonth.plusDays(i);
+
+            // Check if the current date is a weekend (Saturday or Sunday)
+            if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY || currentDate.getDayOfWeek() == DayOfWeek.SUNDAY) {                
+            	holidays++;
+            } else {
+                workdays++;
+            }
+        }
+        
+        int[] dayCountResult = { holidays, workdays } ;
+        
+//        System.out.println("Workdays: " + workdays);
+//        System.out.println("Holidays: " + holidays);
+        
+        return dayCountResult;
+    }
 }
+
+
